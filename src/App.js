79e91fdc73d8a38d938361f402484bba48bf2849
ParkhiSaini <parkhi.saini2401@gmail.com>
2023-04-22 17:11:03 +0530

    updated projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Title from './Components/Title ';
 import Project from './Components/Project';
+
 function App() {
   return (
     <div>
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-
+import whackafood from "../Images/Whack-A-Food.mp4";
 function Project(){
   return(
   <div class = "Project">
@@ -23,6 +23,11 @@ function Project(){
         <a href="https://parkhisaini.itch.io/whack-a-food" >AVAILABLE ON WEB BROWSER</a>
         </div>
       </div> 
+      <div class="videosec">
+      <video width="900" height="390"  onmouseover="this.play()" onmouseout="this.pause();this.currentTime=0;">
+      <source src={whackafood} type="video/mp4"></source>
+      </video>
+      </div>
     
 
     </div>
