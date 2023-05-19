f6897619027017705b02dad6a8a31f9995540b82
Parkhi Saini <90965313+ParkhiSaini@users.noreply.github.com>
2023-05-19 20:29:37 +0530

    completed projects section basic outline

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,38 +1,67 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import whackafood from "../Images/Whack-A-Food.mp4";
-function Project(){
-  return(
-  <div class = "Project">
-    <p class="ptitle">PROJECTS MADE SO FAR</p>
-    <hr  width="30%"  /> 
-    <div class="projects">
-      <div class="square" >
-        <div class= "tsq">
-          <p class="title">WHACK-A-FOOD</p>
-        </div>
-        <div class="about">
-          <p>Unity Engine</p>
-          <p>3D</p>
-          <p>Kind of Fruit Ninja</p>
-          <p>Different difficulty modes</p>
-          
 
-        </div>
-        <div class="browser">
-        <a href="https://parkhisaini.itch.io/whack-a-food" >AVAILABLE ON WEB BROWSER</a>
-        </div>
-      </div> 
-      <div class="videosec">
-      <video width="900" height="390"  onmouseover="this.play()" onmouseout="this.pause();this.currentTime=0;">
-      <source src={whackafood} type="video/mp4"></source>
-      </video>
+const projectsData = [
+  {
+    title: 'WHACK-A-FOOD (2023) ',
+    description: ['3D','Unity Engine', 'Kind of Fruit Ninja', 'Different difficulty modes'],
+    link: 'https://parkhisaini.itch.io/whack-a-food',
+    videoSrc: whackafood
+  },
+  {
+    title: 'SHOOT-THROUGH-TIME (2023)',
+    description: ['3D','Unity Engine', 'Third person shooter', 'Complete the objectives for a succesfful mission'],
+    link: 'https://parkhisaini.itch.io/whack-a-food',
+    videoSrc: whackafood
+  },
+  {
+    title: 'SHOOT-THROUGH-TIME (2023)',
+    description: ['3D','Unity Engine', 'Third person shooter', 'Complete the objectives for a succesfful mission'],
+    link: 'https://parkhisaini.itch.io/whack-a-food',
+    videoSrc: whackafood
+  },
+  {
+    title: 'SHOOT-THROUGH-TIME (2023)',
+    description: ['3D','Unity Engine', 'Third person shooter', 'Complete the objectives for a succesfful mission'],
+    link: 'https://parkhisaini.itch.io/whack-a-food',
+    videoSrc: whackafood
+  }
+
+  // Add more project objects here...
+];
+
+function Project() {
+  return (
+    <div className="Project">
+      <p className="ptitle">PROJECTS MADE SO FAR</p>
+      <hr className="divider" />
+      <div className="projects">
+        {projectsData.map((project, index) => (
+          <div className={`square ${index % 2 === 1 ? 'right' : 'left'}`} key={index}>
+            <div className="tsq">
+              <p className="title">{project.title}</p>
+            </div>
+            <div className="about">
+              {project.description.map((item, i) => (
+                <p key={i}>{item}</p>
+              ))}
+            </div>
+            <div className="browser">
+              <a href={project.link}>AVAILABLE ON WEB BROWSER</a>
+            </div>
+            <div className="videosec">
+              <video width="900" height="390" onMouseOver={e => e.target.play()} onMouseOut={e => {e.target.pause(); e.target.currentTime = 0;}}>
+                <source src={project.videoSrc} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        ))}
       </div>
-    
-
+      <div className="viewmore">
+        <a href="https://parkhisaini.itch.io/"> --VIEW MORE--</a>
+      </div>
     </div>
-    
-  </div >
   );
 }
 
